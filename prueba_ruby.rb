bd66cc2566c68787d0fc375dd97be3820360d617
jano2608 <boris.mora.barra@gmail.com>
2019-08-21 11:24:05 -0400

    option 3

--- prueba_ruby.rb
+++ prueba_ruby.rb
@@ -27,33 +27,55 @@ def student_ubsent (absent_student)
         caunt +=1
       end
     end
-    puts "#{absent[0]} tiene #{caunt} inasistencias"
+    puts "#{absent[0]} tiene #{caunt} en el dia #{index}inasistencias"
+  end
+end
+#3
+def student_no_fail(student_avarage , approved)
+  student_avarage.each do |student|
+    suma =0
+    avarage=0
+    aux=5
+    student.each_with_index do |grades , index|
+      if index!=0
+        suma +=grades.to_i
+        avarage=suma.to_f/index
+      end
+    end
+    if approved == 0
+      approved +=aux
+    end
+    if avarage >= approved
+      puts "  #{student[0]}  #{avarage} "
+    end
   end
 end
 
 
 
 
-student=read_alum("alumnos.csv")
-option = 0
-while option!=4
-   puts "ingrese una opcion : "
-   puts "opcion 1 : mostrar promedio de notas de cada alumno"
-   puts "opcion 2 : mostrar inasistencias de cada alumno "
-   puts "opcion 3 : mostrar alumnos aprobados "
-   puts "opcion 4 salir "
-   puts "---------------------------------------------------------------"
-   option=gets.chomp.to_i
-   case option
-     when 1
+  student=read_alum("alumnos.csv")
+  option = 0
+  while option!=4
+    puts "ingrese una opcion : "
+    puts "opcion 1 : mostrar promedio de notas de cada alumno"
+    puts "opcion 2 : mostrar inasistencias de cada alumno "
+    puts "opcion 3 : mostrar alumnos aprobados "
+    puts "opcion 4 salir "
+    puts "---------------------------------------------------------------"
+    option=gets.chomp.to_i
+    case option
+    when 1
       avarage(student)
-     when 2
+    when 2
       student_ubsent(student)
-     when 3
-      student_no_fail(student)
+    when 3
+      puts "ingrese una nota"
+      approved=gets.chomp.to_i
+      student_no_fail(student , approved)
     when 4
       puts "exit"
-     else
+    else
       puts "opcion no valida"
-   end
+    end
 end
